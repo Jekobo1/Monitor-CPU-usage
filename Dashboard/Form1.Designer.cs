e8f1e12b99c203aa5a6ce5c9c8f1cfb7758cfb91
Jekobo1 <80896722+Jekobo1@users.noreply.github.com>
2021-10-29 22:02:21 +0300

    Add files via upload

---
+++ Dashboard/Form1.Designer.cs
@@ -0,0 +1,271 @@
+﻿namespace Dashboard
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.circularProgressBar1 = new CircularProgressBar.CircularProgressBar();
+            this.circularProgressBar2 = new CircularProgressBar.CircularProgressBar();
+            this.CPU = new System.Diagnostics.PerformanceCounter();
+            this.RAM = new System.Diagnostics.PerformanceCounter();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.TEMP = new System.Diagnostics.PerformanceCounter();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.iPToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.batteryToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.batteryChargeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.batteryStatusToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.CPU)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.RAM)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TEMP)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // circularProgressBar1
+            // 
+            this.circularProgressBar1.AnimationFunction = WinFormAnimation.KnownAnimationFunctions.Liner;
+            this.circularProgressBar1.AnimationSpeed = 500;
+            this.circularProgressBar1.BackColor = System.Drawing.Color.Transparent;
+            this.circularProgressBar1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold);
+            this.circularProgressBar1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.circularProgressBar1.InnerColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.circularProgressBar1.InnerMargin = 2;
+            this.circularProgressBar1.InnerWidth = -1;
+            this.circularProgressBar1.Location = new System.Drawing.Point(42, 12);
+            this.circularProgressBar1.MarqueeAnimationSpeed = 2000;
+            this.circularProgressBar1.Name = "circularProgressBar1";
+            this.circularProgressBar1.OuterColor = System.Drawing.Color.Gray;
+            this.circularProgressBar1.OuterMargin = -25;
+            this.circularProgressBar1.OuterWidth = 26;
+            this.circularProgressBar1.ProgressColor = System.Drawing.Color.Turquoise;
+            this.circularProgressBar1.ProgressWidth = 25;
+            this.circularProgressBar1.SecondaryFont = new System.Drawing.Font("Microsoft Sans Serif", 36F);
+            this.circularProgressBar1.Size = new System.Drawing.Size(221, 198);
+            this.circularProgressBar1.StartAngle = 270;
+            this.circularProgressBar1.SubscriptColor = System.Drawing.Color.FromArgb(((int)(((byte)(166)))), ((int)(((byte)(166)))), ((int)(((byte)(166)))));
+            this.circularProgressBar1.SubscriptMargin = new System.Windows.Forms.Padding(10, -35, 0, 0);
+            this.circularProgressBar1.SubscriptText = "";
+            this.circularProgressBar1.SuperscriptColor = System.Drawing.Color.FromArgb(((int)(((byte)(166)))), ((int)(((byte)(166)))), ((int)(((byte)(166)))));
+            this.circularProgressBar1.SuperscriptMargin = new System.Windows.Forms.Padding(10, 35, 0, 0);
+            this.circularProgressBar1.SuperscriptText = "";
+            this.circularProgressBar1.TabIndex = 0;
+            this.circularProgressBar1.TextMargin = new System.Windows.Forms.Padding(8, 8, 0, 0);
+            this.circularProgressBar1.Value = 68;
+            // 
+            // circularProgressBar2
+            // 
+            this.circularProgressBar2.AnimationFunction = WinFormAnimation.KnownAnimationFunctions.Liner;
+            this.circularProgressBar2.AnimationSpeed = 500;
+            this.circularProgressBar2.BackColor = System.Drawing.Color.Transparent;
+            this.circularProgressBar2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold);
+            this.circularProgressBar2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.circularProgressBar2.InnerColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.circularProgressBar2.InnerMargin = 2;
+            this.circularProgressBar2.InnerWidth = -1;
+            this.circularProgressBar2.Location = new System.Drawing.Point(402, 12);
+            this.circularProgressBar2.MarqueeAnimationSpeed = 2000;
+            this.circularProgressBar2.Name = "circularProgressBar2";
+            this.circularProgressBar2.OuterColor = System.Drawing.Color.Gray;
+            this.circularProgressBar2.OuterMargin = -25;
+            this.circularProgressBar2.OuterWidth = 26;
+            this.circularProgressBar2.ProgressColor = System.Drawing.Color.Turquoise;
+            this.circularProgressBar2.ProgressWidth = 25;
+            this.circularProgressBar2.SecondaryFont = new System.Drawing.Font("Microsoft Sans Serif", 36F);
+            this.circularProgressBar2.Size = new System.Drawing.Size(221, 198);
+            this.circularProgressBar2.StartAngle = 270;
+            this.circularProgressBar2.SubscriptColor = System.Drawing.Color.FromArgb(((int)(((byte)(166)))), ((int)(((byte)(166)))), ((int)(((byte)(166)))));
+            this.circularProgressBar2.SubscriptMargin = new System.Windows.Forms.Padding(10, -35, 0, 0);
+            this.circularProgressBar2.SubscriptText = "";
+            this.circularProgressBar2.SuperscriptColor = System.Drawing.Color.FromArgb(((int)(((byte)(166)))), ((int)(((byte)(166)))), ((int)(((byte)(166)))));
+            this.circularProgressBar2.SuperscriptMargin = new System.Windows.Forms.Padding(10, 35, 0, 0);
+            this.circularProgressBar2.SuperscriptText = "";
+            this.circularProgressBar2.TabIndex = 1;
+            this.circularProgressBar2.TextMargin = new System.Windows.Forms.Padding(8, 8, 0, 0);
+            this.circularProgressBar2.Value = 68;
+            // 
+            // CPU
+            // 
+            this.CPU.CategoryName = "Processor";
+            this.CPU.CounterName = "% Processor Time";
+            this.CPU.InstanceName = "_Total";
+            // 
+            // RAM
+            // 
+            this.RAM.CategoryName = "Memory";
+            this.RAM.CounterName = "% Committed Bytes In Use";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(119, 215);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "CPU";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(486, 215);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "RAM";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(257, 215);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "label3";
+            // 
+            // TEMP
+            // 
+            this.TEMP.CategoryName = "Thermal Zone Information";
+            this.TEMP.CounterName = "Temperature";
+            this.TEMP.InstanceName = "\\_TZ.TZ00";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.iPToolStripMenuItem,
+            this.batteryToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(112, 48);
+            // 
+            // iPToolStripMenuItem
+            // 
+            this.iPToolStripMenuItem.Name = "iPToolStripMenuItem";
+            this.iPToolStripMenuItem.Size = new System.Drawing.Size(111, 22);
+            this.iPToolStripMenuItem.Text = "IP";
+            this.iPToolStripMenuItem.Click += new System.EventHandler(this.iPToolStripMenuItem_Click);
+            // 
+            // batteryToolStripMenuItem
+            // 
+            this.batteryToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.batteryChargeToolStripMenuItem,
+            this.batteryStatusToolStripMenuItem});
+            this.batteryToolStripMenuItem.Name = "batteryToolStripMenuItem";
+            this.batteryToolStripMenuItem.Size = new System.Drawing.Size(111, 22);
+            this.batteryToolStripMenuItem.Text = "Battery";
+            // 
+            // batteryChargeToolStripMenuItem
+            // 
+            this.batteryChargeToolStripMenuItem.Name = "batteryChargeToolStripMenuItem";
+            this.batteryChargeToolStripMenuItem.Size = new System.Drawing.Size(150, 22);
+            this.batteryChargeToolStripMenuItem.Text = "Battery charge";
+            this.batteryChargeToolStripMenuItem.Click += new System.EventHandler(this.batteryChargeToolStripMenuItem_Click);
+            // 
+            // batteryStatusToolStripMenuItem
+            // 
+            this.batteryStatusToolStripMenuItem.Name = "batteryStatusToolStripMenuItem";
+            this.batteryStatusToolStripMenuItem.Size = new System.Drawing.Size(150, 22);
+            this.batteryStatusToolStripMenuItem.Text = "Battery status";
+            this.batteryStatusToolStripMenuItem.Click += new System.EventHandler(this.batteryStatusToolStripMenuItem_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(280, 167);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(279, 137);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "label5";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(660, 263);
+            this.ContextMenuStrip = this.contextMenuStrip1;
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.circularProgressBar2);
+            this.Controls.Add(this.circularProgressBar1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.CPU)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.RAM)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TEMP)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private CircularProgressBar.CircularProgressBar circularProgressBar1;
+        private CircularProgressBar.CircularProgressBar circularProgressBar2;
+        private System.Diagnostics.PerformanceCounter CPU;
+        private System.Diagnostics.PerformanceCounter RAM;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        public System.Diagnostics.PerformanceCounter TEMP;
+        public System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem iPToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem batteryToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem batteryChargeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem batteryStatusToolStripMenuItem;
+        public System.Windows.Forms.Label label4;
+        public System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Dashboard/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Net;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Management;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Dashboard
+{
+    
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+            
+
+        }
+       
+        ManagementObjectSearcher searcher = new ManagementObjectSearcher();
+        private static string GetBattery()
+        {
+            PowerStatus p = SystemInformation.PowerStatus;
+            int a = (int)(p.BatteryLifePercent * 100);
+            return a.ToString();
+            
+        }
+        private static string GetBatteryStatus()
+        {
+            PowerStatus p = SystemInformation.PowerStatus;
+            int a = (int)(p.BatteryChargeStatus);
+            if (a > 0)
+                return "Charging!";
+            else
+                return "Not charging!";
+            
+
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            float fcpu = CPU.NextValue();
+            float dram = RAM.NextValue();
+            double temp = TEMP.NextValue();
+            circularProgressBar1.Value = (int)fcpu;
+            circularProgressBar1.Text = string.Format("{0:0.00}%", fcpu);
+
+            circularProgressBar2.Value = (int)dram;
+            circularProgressBar2.Text = string.Format("{0:0.00}%", dram);
+
+            label3.Text = "Temperature: " + (temp - 273.15).ToString();
+            label5.Text = (GetTemp() - 273.15).ToString();
+            
+           
+            
+
+        }
+
+        protected static string GetIP()
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var ip in host.AddressList)
+            {
+                if(ip.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+                {
+                    return ip.ToString();
+                }
+                
+            }
+            throw new Exception("No network adapters");
+
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Escape)
+            {
+                this.Close();
+            }
+        }
+
+        private void iPToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(GetIP(), "Your IP Address");
+        }
+
+        private void batteryChargeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(GetBattery() + " %", "Your Battery charge");
+        }
+
+        private void batteryStatusToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(GetBatteryStatus(), "Your Battery status");
+        }
+        private static float GetTemp()
+        {
+            PerformanceCounterCategory performanceCounterCategory = new PerformanceCounterCategory("Thermal Zone Information");
+            var instances = performanceCounterCategory.GetInstanceNames();
+            List<PerformanceCounter> temperatureCounters = new List<PerformanceCounter>();
+            foreach (string instanceName in instances)
+            {
+
+                foreach (PerformanceCounter counter in performanceCounterCategory.GetCounters(instanceName))
+                {
+                    if (counter.CounterName == "Temperature")
+                    {
+                        temperatureCounters.Add(counter);
+                    }
+                }
+            }
+
+
+            while (true)
+            {
+                foreach (PerformanceCounter counter in temperatureCounters)
+                {
+                    //Console.WriteLine("{0} {1} {2} {3}", counter.CategoryName, counter.CounterName, counter.InstanceName, counter.NextValue());
+                    return counter.NextValue();
+                }
+                
+            }
+        }
+
+        
+    }
+}
